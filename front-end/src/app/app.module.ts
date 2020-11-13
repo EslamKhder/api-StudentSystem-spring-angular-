3ac48ee1cf7f013a437e4bc7483005738c1b5576
EslamKhder <eslamkhder81@gmail.com>
2020-11-13 19:10:36 +0200

    Create Component (log in) Closes #10

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componants/header/header.component';
 import { FooterComponent } from './componants/footer/footer.component';
+import { LogInComponent } from './componants/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    LogInComponent,
   ],
   imports: [
     BrowserModule
