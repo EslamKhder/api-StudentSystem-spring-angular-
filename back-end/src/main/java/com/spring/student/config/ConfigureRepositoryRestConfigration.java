e8ae99854d77b9afe866c07578d03019c8541c27
EslamKhder <eslamkhder81@gmail.com>
2020-11-14 01:44:16 +0200

    add ids to entites Closes #21

---
+++ back-end/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfigration.java
@@ -0,0 +1,45 @@
+package com.spring.student.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class ConfigureRepositoryRestConfigration implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public ConfigureRepositoryRestConfigration(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        exposeIds(config);
+    }
+
+    public void exposeIds(RepositoryRestConfiguration config){
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
--- back-end/src/main/java/com/spring/student/doa/StudentRepository.java
+++ back-end/src/main/java/com/spring/student/doa/StudentRepository.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    
 }
