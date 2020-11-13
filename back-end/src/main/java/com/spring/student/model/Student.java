06309ab3c3c96a5dee312c5e553274f6ec420c99
EslamKhder <eslamkhder81@gmail.com>
2020-11-13 10:00:41 +0200

    create Student model Closes #1

---
+++ back-end/src/main/java/com/spring/student/model/Student.java
@@ -0,0 +1,25 @@
+package com.spring.student.model;
+
+import com.spring.student.model.enums.Gender;
+
+import javax.xml.crypto.Data;
+
+public class Student {
+
+    private int id;
+
+    private String fullName;
+
+    private Gender gender;
+
+    private String phone;
+
+    private String age;
+
+    private String address;
+
+    private Data datacreated;
+
+    private Data dataUpdated;
+
+}
---
+++ back-end/src/main/java/com/spring/student/model/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.spring.student.model.enums;
+
+public enum Gender {
+    MALE,FEMALE
+}
