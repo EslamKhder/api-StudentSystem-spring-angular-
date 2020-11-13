c8a0a1b90c04dc1ca960fc2f4fa17ddd4e716dcb
EslamKhder <eslamkhder81@gmail.com>
2020-11-13 21:14:31 +0200

    Create Model Student Closes #16

--- front-end/src/app/model/student.ts
+++ front-end/src/app/model/student.ts
@@ -1,23 +1,22 @@
 export class Student {
-
   id: number;
   fullName: string;
-  gender: String;
+  gender: string;
   age: string;
-  address: string;
   phone: string;
-  dataCreated: Date;
-  dataUpdated: Date;
+  address: string;
+  dateCreated: Date;
+  dateUpdated: Date;
 
 
-  constructor(id: number, fullName: string, gender: String, age: string, address: string, phone: string, dataCreated: Date, dataUpdated: Date) {
+  constructor(id: number, fullName: string, gender: string, age: string, phone: string, address: string, dateCreated: Date, dateUpdated: Date) {
     this.id = id;
     this.fullName = fullName;
     this.gender = gender;
     this.age = age;
-    this.address = address;
     this.phone = phone;
-    this.dataCreated = dataCreated;
-    this.dataUpdated = dataUpdated;
+    this.address = address;
+    this.dateCreated = dateCreated;
+    this.dateUpdated = dateUpdated;
   }
 }
