9e36bc5e53aa088baa68c2a6384e9dd86fb13096
EslamKhder <eslamkhder81@gmail.com>
2021-02-07 04:12:37 +0200

    finish

--- back-end/src/main/java/com/spring/student/model/AuthenticationBean.java
+++ back-end/src/main/java/com/spring/student/model/AuthenticationBean.java
@@ -1,17 +1,12 @@
 package com.spring.student.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationBean {
-    private String message;
-
-    public AuthenticationBean(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private String token;
 }
